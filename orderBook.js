0c4a97fc00f4bc6ed518e88c7a059119c1eb8b83
Ikponmwosa <otasowieik@gmail.com>
2021-11-16 18:42:02 -0500

    added third function for correspondingtypes but not matching quantities

---
+++ orderBook.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-console */
+/* 1. This function compares an incoming transaction order to orders 
+    placed in an existing order book and acts accoringly after comparing them.
+2. This function accepts 1 array and 1 object
+3. This function returns an array
+*/
+
+
+
+// create function test is calling for
+const reconcileOrder = (existingBook, incomingOrder) => {
+// adds an order to book when book is empty
+  if (existingBook.length === 0) {
+    return emptyBookOrder(existingBook, incomingOrder)
+  }
+
+  // adds an order to book when incomingOrder type + existingBook  
+  // types matches IE; when BOTH types are sell or buy
+  for (let index = 0; index < existingBook.length; index++) {
+    if (incomingOrder.type === existingBook[index].type) {
+      return matchingTypes(existingBook, incomingOrder)
+    }
+  }
+
+  for (let index = 0; index < existingBook.length; index++) {
+    if (incomingOrder.type !== existingBook[index].type && incomingOrder.quantity !== existingBook[index].quantity) {
+      return correspondingTypes(existingBook, incomingOrder)
+    }
+  }
+}
+
+
+
+const emptyBookOrder = (emptyExistingBook, incomingOrder) => {
+  emptyExistingBook.push(incomingOrder)
+
+  return emptyExistingBook
+}
+
+const matchingTypes = (existingBook, incomingOrder) => {
+  existingBook.push(incomingOrder)
+
+  return existingBook
+}
+
+const correspondingTypes = (existingBook, incomingOrder) => {
+  existingBook.push(incomingOrder)
+
+  return existingBook
+}
+
+
+
+// export the function being called for
+module.exports = reconcileOrder
